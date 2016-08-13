254f4d01cd5d667c63c8f337030095885e4d467b
CodeRed <zeketiki@gmail.com>
2016-08-12 23:15:06 -0700

    Fixed the update for UserDetails by encrypting the password and including it as a hidden element. Finilized the car make and model search.

--- Auto-Barter/Auto-Barter/Controllers/DefaultController.cs
+++ Auto-Barter/Auto-Barter/Controllers/DefaultController.cs
@@ -24,12 +24,6 @@ namespace Auto_Barter.Controllers
 
             List<SelectListItem> items = new List<SelectListItem>();
 
-            items.Add(new SelectListItem
-            {
-                Text = "Choose a make",
-                Value = null
-            });
-
             foreach (var item in content.makes)
             {
                 items.Add(new SelectListItem
@@ -50,25 +44,16 @@ namespace Auto_Barter.Controllers
             List<string> ModelNames = new List<string>();
             var serialiser = new JavaScriptSerializer();
 
-            if (make == "Choose a make")
+            string apiKey = "968v7jffe8uw4teunhppq5r5";
+            var client = new RestClient("http://api.edmunds.com/api/vehicle/v2/");
+            var request = new RestRequest($"{make}?&view=basic&fmt=json&api_key={apiKey}");
+            request.OnBeforeDeserialization = resp => { resp.ContentType = "application/json"; };
+            var response = client.Execute(request);
+            var content = JsonConvert.DeserializeObject<makes>(response.Content);
+            foreach (var item in content.models)
             {
-                ModelNames.Clear();
-            }
-            else
-            {
-                ModelNames.Add("Choose a model");
+                ModelNames.Add(item.name);
 
-                string apiKey = "968v7jffe8uw4teunhppq5r5";
-                var client = new RestClient("http://api.edmunds.com/api/vehicle/v2/");
-                var request = new RestRequest($"{make}?&view=basic&fmt=json&api_key={apiKey}");
-                request.OnBeforeDeserialization = resp => { resp.ContentType = "application/json"; };
-                var response = client.Execute(request);
-                var content = JsonConvert.DeserializeObject<makes>(response.Content);
-                foreach (var item in content.models)
-                {
-                    ModelNames.Add(item.name);
-
-                }
             }
 
             var models = serialiser.Serialize(ModelNames);
